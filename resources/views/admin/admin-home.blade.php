c7232a8081eb3e3154ff5cd9ea83621210b1a603
ramapersonalproject <workmailrama@gmail.com>
2024-06-26 00:37:54 +0700

    admin1.0

---
+++ resources/views/admin/admin-home.blade.php
@@ -0,0 +1,1 @@
+@extends('admin.admin-components.admin-layout')
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+
+                //admin css
+                'resources/plugins/fontawesome-free/css/all.min.css',
+                'resources/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+                'resources/dist/css/adminlte.min.css',
+                //admin js
+                'resources/plugins/jquery/jquery.min.js',
+                'resources/plugins/bootstrap/js/bootstrap.bundle.min.js',
+                'plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+                'resources/dist/js/adminlte.js'
+            ],
             refresh: true,
         }),
     ],
